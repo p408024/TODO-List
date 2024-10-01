5bd901c0f714882b831e2806e1cd691783840c57
p408024 <p408024@protonmail.com>
2024-10-01 11:59:08 +0200

    Created initial code setup branch

---
+++ main.js
@@ -0,0 +1,10 @@
+// HTML References
+let todoElement  = document.getElementById("todo-column")
+let doingElement = document.getElementById("doing-column")
+let doneElement  = document.getElementById("done-column")
+
+// Global state
+let tasks = [[32],
+             [12],
+             [71]]
+             
