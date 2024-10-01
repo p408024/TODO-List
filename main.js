7e514c0ef88083e6a091d022571bce0399e0f4ab
p408024 <p408024@protonmail.com>
2024-10-01 12:35:26 +0200

    Implemented Render function, added classes to column divs

--- main.js
+++ main.js
@@ -4,7 +4,43 @@ let doingElement = document.getElementById("doing-column")
 let doneElement  = document.getElementById("done-column")
 
 // Global state
-let tasks = [[32],
+let tasks = [[32, 55],
              [12],
              [71]]
              
+// Updates task columns HTML, should be called every time "tasks" is modified
+function Render() {
+    // Clear columns
+    todoElement.innerHTML  = ""
+    doingElement.innerHTML = ""
+    doneElement.innerHTML  = ""
+
+    // Add a task card HTML element for every task in "tasks" array
+    tasks[0].forEach(task => {
+        todoElement.innerHTML += `
+            <div style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
+                <button style="width: 100%">⬅️</button>
+                <h1 style="margin: .5rem;">${task}</h1>
+                <button style="width: 100%">➡️</button>
+            </div>`
+    });
+
+    tasks[1].forEach(task => {
+        doingElement.innerHTML += `
+            <div style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
+                <button style="width: 100%">⬅️</button>
+                <h1 style="margin: .5rem;">${task}</h1>
+                <button style="width: 100%">➡️</button>
+            </div>`
+    });
+
+    tasks[2].forEach(task => {
+        doneElement.innerHTML += `
+            <div style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
+                <button style="width: 100%">⬅️</button>
+                <h1 style="margin: .5rem;">${task}</h1>
+                <button style="width: 100%">➡️</button>
+            </div>`
+    });
+}
+Render()
