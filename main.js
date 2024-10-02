c9a347bb53ef38ec6ee75441ba7910a1a3815d9a
p408024 <p408024@protonmail.com>
2024-10-02 10:29:54 +0200

    Resolved merge conflicts 3, organized code and fixed NewTask

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let tasks = [[32, 55, 0],
              [2, 3, 12],
              [71, 1]]
 
-// TASK FUNCTIONS
+//#region TASK FUNCTIONS
 // Moves task to the previous or next column
 function MoveTask(taskId, columnId, direction) {
     // Find the task element and remove it
@@ -20,34 +20,41 @@ function MoveTask(taskId, columnId, direction) {
     // Update UI
     Render()
 }
-// setTimeout(() => {
-//     MoveTask(71, 2, -1)
-//     Render()
-// }, 2000);
 
+function NewTask(columnId) {
+    let columnToAdd = task[columnId]
 
-// Helper functions
-function NewTaskWrapper() {
-    console.log("Test");
-    NewTask(tasks[2])
-}
-
-function NewTask(taskType) {
     let newId = 0
     let u = 0
     let i = 0
     while (u == 0) {
         if(tasks[0].includes(i) == false && tasks[1].includes(i) == false && tasks[2].includes(i) == false){
             newId = i
-            taskType.push(newId)
+            columnToAdd.push(newId)
             u = 1
         }
         i++
     }
-    console.log(tasks)
 
     Render()
 }
+
+function deleteTask(taskId) {
+    if(tasks[0].includes(taskId) == true){
+        let position = tasks[0].indexOf(taskId)
+        tasks[0].splice(position, 1)
+    }
+    if(tasks[1].includes(taskId) == true){
+        let position = tasks[1].indexOf(taskId)
+        tasks[1].splice(position, 1)
+    }
+    if(tasks[2].includes(taskId) == true){
+        let position = tasks[2].indexOf(taskId)
+        tasks[2].splice(position, 1)
+    }
+    Render()
+}
+//#endregion
              
 // Updates task columns HTML, should be called every time "tasks" is modified
 function Render() {
@@ -101,21 +108,3 @@ for (i = 0; i < acc.length; i++) {
     }
     });
 }
-
-function deleteTask(taskId) {
-    if(tasks[0].includes(taskId) == true){
-        let position = tasks[0].indexOf(taskId)
-        tasks[0].splice(position, 1)
-    }
-    if(tasks[1].includes(taskId) == true){
-        let position = tasks[1].indexOf(taskId)
-        tasks[1].splice(position, 1)
-    }
-    if(tasks[2].includes(taskId) == true){
-        let position = tasks[2].indexOf(taskId)
-        tasks[2].splice(position, 1)
-    }
-    Render()
-}
-deleteTask(71)
-console.log(tasks)
