205538bee6fd6d9dc23eb72926f9027986d37526
Joel Rodríguez <joelrc.blade@gmail.com>
2024-10-02 13:38:55 +0200

    updated new task function

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let doneElement = document.getElementById("done-column")
 
 // Global state
 let tasks = [
-    [{ id: 20, title: "Comprar ous", description: "anar al mercadona i comprar ous XL" }],
-    [{ id: 53, title: "Comprar pomes", description: "pomespomespomespomes"}],
+    [{ id: 20, title: "Comprar ous", description: "anar al mercadona i comprar ous XL" }, { id: 70, title: "Comprar ous", description: "anar al mercadona i comprar ous XL" }],
+    [{ id: 53, title: "Comprar pomes", description: "pomespomespomespomes" }],
     [{ id: 63, title: "Netejar cuina (lejía)", description: "cuinacuinacuinacuinacuinacuina" }]
 ]
 
@@ -17,9 +17,9 @@ function MoveTask(el, direction) {
     let taskId = el.parentElement.id
     let columnId = 0
     switch (el.parentElement.parentElement.id) {
-        case "todo-column":  columnId = 0; break;
+        case "todo-column": columnId = 0; break;
         case "doing-column": columnId = 1; break;
-        case "done-column":  columnId = 2; break;
+        case "done-column": columnId = 2; break;
     }
 
     // Find the task element and remove it
@@ -31,7 +31,7 @@ function MoveTask(el, direction) {
             tasks[columnId].splice(i, 1)
         }
     }
-    if(taskToMove == {}) return
+    if (taskToMove == {}) return
 
     // Add it to the next column
     let newColumnId = Math.min(Math.max(columnId + direction, 0), 2)
@@ -43,46 +43,65 @@ function MoveTask(el, direction) {
 
 function NewTask(columnId) {
     let columnToAdd = tasks[columnId]
-
     // Create new task data (id)
+    let allId = []
+    for (let i = 0; i < tasks[0].length; i++) {
+        allId.push(tasks[0][i].id)
+    }
+    for (let i = 0; i < tasks[1].length; i++) {
+        allId.push(tasks[1][i].id)
+    }
+    for (let i = 0; i < tasks[2].length; i++) {
+        allId.push(tasks[2][i].id)
+    }
+    console.log(allId)
     let newId = 0
     let u = 0
-    let i = 0
+    let x = 52
     while (u == 0) {
-        if(tasks[0].includes(i) == false && tasks[1].includes(i) == false && tasks[2].includes(i) == false){
-            newId = i
-            columnToAdd.push(newId)
+        if (allId.includes(x) == false) {
+            newId = x
+            console.log(newId)
+            columnToAdd.push({id: newId, title: "Hola", description: "aloh"})
             u = 1
+            console.log(columnToAdd)
         }
-        i++
+        x++
+        // if(columnToAdd[0].id == false && columnToAdd[0].id.includes(i) == false && columnToAdd[0].id.includes(i) == false){
+        // newId = i
+        // columnToAdd.push(newId)
+        // console.log(columnToAdd)
+        // u = 1
+        //}
+        //i++
     }
 
     Render()
 }
 
 function deleteTask(taskId) {
-    if(tasks[0].includes(taskId) == true){
+    if (tasks[0].includes(taskId) == true) {
         let position = tasks[0].indexOf(taskId)
         tasks[0].splice(position, 1)
     }
-    if(tasks[1].includes(taskId) == true){
+    if (tasks[1].includes(taskId) == true) {
         let position = tasks[1].indexOf(taskId)
         tasks[1].splice(position, 1)
     }
-    if(tasks[2].includes(taskId) == true){
+    if (tasks[2].includes(taskId) == true) {
         let position = tasks[2].indexOf(taskId)
         tasks[2].splice(position, 1)
     }
     Render()
 }
 //#endregion
-             
+
 // Updates task columns HTML, should be called every time "tasks" is modified
 function Render() {
     // Clear columns
-    todoElement.innerHTML  = todoElement.getElementsByClassName("column-title")[0].outerHTML
+    todoElement.innerHTML = todoElement.getElementsByClassName("column-title")[0].outerHTML
     doingElement.innerHTML = doingElement.getElementsByClassName("column-title")[0].outerHTML
-    doneElement.innerHTML  = doneElement.getElementsByClassName("column-title")[0].outerHTML
+    doneElement.innerHTML = doneElement.getElementsByClassName("column-title")[0].outerHTML
 
     // Add a task card HTML element for every task in "tasks" array
     tasks[0].forEach(task => {
@@ -113,7 +132,7 @@ function Render() {
     });
 }
 Render()
-             
+
 // Task accordion behaviour
 // var acc = document.getElementsByClassName("accordion");
 // var i;
