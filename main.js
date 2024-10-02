42cf8fd8e46db3ad177413d430c057f38c112d6a
p408024 <p408024@protonmail.com>
2024-10-02 11:15:29 +0200

    Fixed MoveTask, now HTML reflects changes

--- main.js
+++ main.js
@@ -10,10 +10,24 @@ let tasks = [[32, 55, 0],
 
 //#region TASK FUNCTIONS
 // Moves task to the previous or next column
-function MoveTask(taskId, columnId, direction) {
+function MoveTask(el, direction) {
+    let taskId = el.parentElement.id
+    let columnId = 0
+    switch (el.parentElement.parentElement.id) {
+        case "todo-column":  columnId = 0; break;
+        case "doing-column": columnId = 1; break;
+        case "done-column":  columnId = 2; break;
+    }
+
     // Find the task element and remove it
-    // tasks[columnId] = tasks[columnId].filter(task => task != taskId) // TODO - Check if this inequality also works with objects instead of integers
-    tasks[columnId].splice(tasks[columnId].indexOf(taskId), 1)
+    
+    for (let i = 0; i < tasks[columnId].length; i++) {
+        // console.log("comparing " + taskId + " with " + tasks[columnId][i]);
+        if (tasks[columnId][i] == taskId) {
+            tasks[columnId].splice(i, 1)
+        }
+    }
+
     // Add it to the next column
     let newColumnId = Math.min(Math.max(columnId + direction, 0), 2)
     tasks[newColumnId].push(taskId)
@@ -22,7 +36,7 @@ function MoveTask(taskId, columnId, direction) {
 }
 
 function NewTask(columnId) {
-    let columnToAdd = task[columnId]
+    let columnToAdd = tasks[columnId]
 
     let newId = 0
     let u = 0
@@ -66,28 +80,28 @@ function Render() {
     // Add a task card HTML element for every task in "tasks" array
     tasks[0].forEach(task => {
         todoElement.innerHTML += `
-            <div style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
-                <button style="width: 100%">⬅️</button>
+            <div id=${task} style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
+                <button style="width: 100%" onClick="MoveTask(this, -1)">⬅️</button>
                 <h1 style="margin: .5rem;">${task}</h1>
-                <button style="width: 100%">➡️</button>
+                <button style="width: 100%" onClick="MoveTask(this, 1)">➡️</button>
             </div>`
     });
 
     tasks[1].forEach(task => {
         doingElement.innerHTML += `
-            <div style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
-                <button style="width: 100%">⬅️</button>
+            <div id=${task} style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
+                <button style="width: 100%" onclick="MoveTask(this, -1)">⬅️</button>
                 <h1 style="margin: .5rem;">${task}</h1>
-                <button style="width: 100%">➡️</button>
+                <button style="width: 100%" onclick="MoveTask(this, 1)">➡️</button>
             </div>`
     });
 
     tasks[2].forEach(task => {
         doneElement.innerHTML += `
-            <div style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
-                <button style="width: 100%">⬅️</button>
+            <div id=${task} style="background-color: beige; display: flex; flex-direction: horizontal; margin: .5rem; padding: 1rem; justify-content: center;">
+                <button style="width: 100%" onclick="MoveTask(this, -1)">⬅️</button>
                 <h1 style="margin: .5rem;">${task}</h1>
-                <button style="width: 100%">➡️</button>
+                <button style="width: 100%" onclick="MoveTask(this, 1)">➡️</button>
             </div>`
     });
 }
